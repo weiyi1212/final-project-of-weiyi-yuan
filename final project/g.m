c9d1f8c5433529ad0deeecf92166b7929afbfe33
weiyi1212 <82320191+weiyi1212@users.noreply.github.com>
2022-04-25 11:00:22 +0800

    Update g.m

--- final project/g.m
+++ final project/g.m
@@ -1,5 +1,5 @@
 % % simple person to person model
-% % % Q=fg(r,r0,r1)t;Q=1的剂量对应63%的感染概率
+% % % Q=fg(r,r0,r1)t;Q=1
 % % % f is a constant[1,400](for hour), g(r) is function of distance r, t is time(hour)
 % % % g(r,r0,r1)=1/r2 when r0<=r<=r1 , g(r)=1 when r<r0, g(r)=0 when r>r1
 % % % 
@@ -8,7 +8,7 @@ function Q=g(r,r0,r1)
 if r<r0
     Q=1;
 elseif r<=r1
-   Q=1/r/r;
+   Q=1/r;
 else
    Q=0;
 end
