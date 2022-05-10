1d755f62d0ce44d13564ad91d1c41a5140ead827
weiyi1212 <82320191+weiyi1212@users.noreply.github.com>
2022-05-10 10:53:48 +0800

    Update g.m

--- final project/g.m
+++ final project/g.m
@@ -1,7 +1,7 @@
 % % simple person to person model
 % % % Q=fg(r,r0,r1)t;Q=1
 % % % f is a constant[1,400](for hour), g(r) is function of distance r, t is time(hour)
-% % % g(r,r0,r1)=1/r2 when r0<=r<=r1 , g(r)=1 when r<r0, g(r)=0 when r>r1
+% % % g(r,r0,r1)=1/r when r0<=r<=r1 , g(r)=1 when r<r0, g(r)=0 when r>r1
 % % % 
 
 function Q=g(r,r0,r1)
